7da52a69a9823f6008fd97db8b2efe6e34e4e937
mocapapa@g.pugpug.org <mocapapa@g.pugpug.org@2934bdbe-dc29-11dd-b4f8-6d137782c98b>
2010-10-04 07:58:15 +0000

    updated

--- requirements/messages/ja/yii.php
+++ requirements/messages/ja/yii.php
@@ -16,6 +16,9 @@
  * @version $Id: yii.php,v 1.2 2009-01-16 18:45:59+09 mocapapa Exp mocapapa $
  */
 return array (
+  'GD extension with<br />FreeType support'=>'GD拡張 (FreeTypeサポート)',
+  'GD installed<br />FreeType support not installed' => 'GDはインストールされています。<br />FreeTypeサポートはインストールされていません。',
+  'GD not installed' => 'GDがインストールされていません。',
   '$_SERVER does not have {vars}.' => '$_SERVERは{vars}を持ちません。',
   '$_SERVER variable' => '$_SERVER変数',
   '$_SERVER["SCRIPT_FILENAME"] must be the same as the entry script file path.' => '$_SERVER["SCRIPT_FILENAME"]はエントリスクリプトのパスと同じであるべきです。',
@@ -23,7 +26,6 @@ return array (
   'All <a href="http://www.yiiframework.com/doc/api/#system.db">DB-related classes</a>' => '全ての<a href="http://www.yiiframework.com/doc/api/#system.db">データベース関連クラス</a>',
   'DOM extension' => 'DOM拡張',
   'Either $_SERVER["REQUEST_URI"] or $_SERVER["QUERY_STRING"] must exist.' => '$_SERVER["REQUEST_URI"]か$_SERVER["QUERY_STRING"]が存在するべきです。',
-  'GD extension' => 'GD拡張',
   'Mcrypt extension' => 'Mcrypt拡張',
   'Memcache extension' => 'Memcache拡張',
   'PCRE extension' => 'PCRE拡張',
@@ -41,7 +43,4 @@ return array (
   'This is required if you are using PostgreSQL database.' => 'これはPostgreSQLデータベースをお使いなら必要です。',
   'This is required if you are using SQLite database.' => 'これはSQLiteデータベースをお使いなら必要です。',
   'Unable to determine URL path info. Please make sure $_SERVER["PATH_INFO"] (or $_SERVER["PHP_SELF"] and $_SERVER["SCRIPT_NAME"]) contains proper value.' => 'URLパス情報を決定できません。$_SERVER["PATH_INFO"](もしくは$_SERVER["PHP_SELF"]と$_SERVER["SCRIPT_NAME"])が正しい値かどうか確認してください。',
-  'GD extension with<br />FreeType support'=>'GD拡張 (FreeTypeサポート)',
-  'GD installed<br />FreeType support not installed' => 'GDはインストールされています。<br />FreeTypeサポートはインストールされていません。',
-  'GD not installed' => 'GDがインストールされていません。',
 );
