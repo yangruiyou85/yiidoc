c4a127d7de6d0b6af1ec0e8804fb8a0c3c37f31c
dr.muaid <dr.muaid@2934bdbe-dc29-11dd-b4f8-6d137782c98b>
2011-04-18 11:45:26 +0000

    [ar] Fixing a UTF-8 BOM problem and html meta-data/attr in requirements trans.

--- trunk/requirements/views/ar/index.php
+++ trunk/requirements/views/ar/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ar" lang="ar">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<meta http-equiv="content-language" content="en"/>
+<meta http-equiv="content-language" content="ar"/>
 <link rel="stylesheet" type="text/css" href="css/main.css" />
 <title>مدقق متطلبات Yii</title>
 </head>
